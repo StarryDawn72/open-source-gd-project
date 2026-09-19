3e56633e5602aeab6f1f6c30a4c4e2756c8b07ad
StarryDawn72 <artin.g4000@gmail.com>
2026-09-18 21:51:48 -0400

    fix postCollision
    Renamed variables for clarity and updated conditions in preCollision method.

--- src/PlayerObject/preCollision.cpp
+++ src/PlayerObject/preCollision.cpp
@@ -1,13 +1,17 @@
-// UNTESTED
-void PlayerObject::preCollision() {
+void PlayerObject::preCollision() 
+{
+    // Rename incorrect bindings
     bool& m_onGround = m_isOnGround2;
     bool& m_ringJumpingThisTick = m_ringJumpRelated;
     int& m_slopeSlideTimer = m_maybeSlidingTime;
     double& m_slopeForceEndTime = m_maybeSlidingStartTime;
     GameObject*& m_preSlopeObject = m_currentPotentialSlope;
-    std::unordered_map<int, GameObject*>& m_preSlopeObjects = m_potentialSlopeMap;
+    gd::unordered_map<int, GameObject*>& m_preSlopeObjects = m_potentialSlopeMap;
+    bool& m_slopeForceLeft = m_platformerMovingLeft;
+    bool& m_slopeForceRight = m_platformerMovingRight;
+    bool& m_shouldDisconnectSlope = m_unk669;
 
-    m_isOnGround4 = m_onGround;
+    m_isOnGround4 = m_onGround; // TODO: rename m_isOnGround4
     m_ringJumpingThisTick = false;
     m_wasOnSlope = m_isOnSlope;
     m_isOnSlope = false;
@@ -17,15 +21,15 @@ void PlayerObject::preCollision() {
     m_collidedLeftMaxX = 0.0;
     m_collidedRightMinX = 0.0;
 
-    if (m_onGround && !m_platformerMovingLeft && !m_platformerMovingRight && m_slopeSlideTimer > 0) {
+    if (m_onGround && !m_slopeForceLeft && !m_slopeForceRight && m_slopeSlideTimer > 0) {
         m_slopeSlideTimer = 0;
         m_slopeForceEndTime = m_totalTime;
     }
 
-    if (m_unk669)
+    if (m_shouldDisconnectSlope)
         m_preSlopeObject = nullptr;
     else
-        m_unk669 = true;
+        m_shouldDisconnectSlope = true;
 
     m_preSlopeObjects.clear();
 
